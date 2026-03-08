7b27a428ce28e17a2a7983c5f036afec4dc873f0
Ayush Saha <ayushsaha.work@gmail.com>
2026-03-08 16:03:50 +0530

    Implement second approach for single number problem
    Added a second approach using bitwise XOR to find the single number.

--- HashMap & HashSet/136. Single Number.cpp
+++ HashMap & HashSet/136. Single Number.cpp
@@ -1,3 +1,4 @@
+//Approach 1
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -14,5 +15,16 @@ public:
         }
 
         return -1;
+    }
+};
+//Approach 2
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+           ans = ans^nums[i];
+        }
+        return ans;
     }
 };
